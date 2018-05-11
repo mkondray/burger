3eec5603cea5485c0aec50abd622374e7c98b920
mkondray <mkondray@gmail.com>
2018-05-11 16:26:44 -0400

    heroku functionality

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ("Black Burger");
+INSERT INTO burgers(burger_name) VALUES ("Blue Burger");
+INSERT INTO burgers(burger_name) VALUES ("Bacon Burger");
+INSERT INTO burgers(burger_name) VALUES ("Baby Back Burger");
+INSERT INTO burgers(burger_name) VALUES ("Bob's  Burger");
